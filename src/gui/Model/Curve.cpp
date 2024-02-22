e9832ac4a837c91761363f545c3419c7da4eb93f
笛鹿FlutyDeer <flutydeer3968@outlook.com>
2024-02-23 01:15:24 +0800

    Impl pitch param editing

--- src/gui/Model/Curve.cpp
+++ src/gui/Model/Curve.cpp
@@ -4,6 +4,9 @@
 
 #include "Curve.h"
 
+#include <QPointF>
+#include <QDebug>
+
 int Curve::start() const {
     return m_start;
 }
@@ -32,11 +35,92 @@ const QList<int> &DrawCurve::values() const {
 void DrawCurve::setValues(const QList<int> &values) {
     m_values = values;
 }
-void DrawCurve::insertValue(int value) {
+void DrawCurve::insertValue(int index, int value) {
+    m_values.insert(index, value);
+}
+void DrawCurve::insertValues(int index, const QList<int> &values) {
+    for (int i = 0; i < values.count(); i++)
+        m_values.insert(index + i, values.at(i));
+}
+void DrawCurve::removeValueRange(int i, int n) {
+    m_values.remove(i, n);
+}
+void DrawCurve::clearValues() {
+    m_values.clear();
+}
+void DrawCurve::appendValue(int value) {
     m_values.append(value);
+}
+void DrawCurve::replaceValue(int index, int value) {
+    m_values.replace(index, value);
+}
+void DrawCurve::mergeWith(const DrawCurve &other) {
+    auto curStart = start();
+    auto otherStart = other.start();
+    auto curEnd = endTick();
+    auto otherEnd = other.endTick();
+    if (otherStart > curStart && otherEnd < curEnd)
+        return;
+
+    if (otherStart > curStart) {
+        auto startIndex = (curEnd - otherStart) / step;
+        for (int i = startIndex; i < other.values().count(); i++)
+            appendValue(other.values().at(i));
+    } else {
+        QList<int> earlyCurve;
+        auto earlyCurvePointCount = (curStart - otherStart) / step;
+        for (int i = 0; i < earlyCurvePointCount; i++)
+            earlyCurve.append(other.values().at(i));
+        insertValues(0, earlyCurve);
+        setStart(otherStart);
+        if (curEnd < otherEnd) {
+            auto startIndex = (otherEnd - curEnd) / step;
+            for (int i = startIndex; i < other.values().count(); i++)
+                appendValue(other.values().at(i));
+        }
+    }
+}
+void DrawCurve::overlayMergeWith(const DrawCurve &other) {
+    auto curStart = start();
+    auto otherStart = other.start();
+    auto curEnd = endTick();
+    auto otherEnd = other.endTick();
+    qDebug() << "DrawCurve::overlayMergeWith" << otherStart << otherEnd;
+    // Q_ASSERT_X(otherStart <= curEnd && curStart >= otherEnd, "DrawCurve::drawCurve",
+    //            "other curve is not overlapped with current");
+
+    if (otherStart > curStart) {
+        auto editStartIndex = (otherStart - curStart) / step;
+        if (curEnd >= otherEnd) {
+            for (int i = 0; i < other.values().count(); i++)
+                replaceValue(editStartIndex + i, other.values().at(i));
+        } else {
+            removeValueRange(editStartIndex, values().count() - editStartIndex);
+            for (int i = 0; i < other.values().count(); i++)
+                appendValue(other.values().at(i));
+        }
+    } else { // otherStart <= curStart
+        if (otherEnd>=curEnd) {
+            clearValues();
+            setStart(otherStart);
+            for (int i = 0; i < other.values().count(); i++)
+                appendValue(other.values().at(i));
+        }else {//otherEnd<curEnd
+            auto removeEndIndex = (otherEnd - curStart) / step;
+            removeValueRange(0, removeEndIndex);
+            setStart(otherStart);
+            insertValues(0, other.values());
+        }
+    }
 }
 int DrawCurve::endTick() const {
     return start() + m_step * m_values.count();
+}
+void ProbeLine::setEndTick(int tick) {
+    m_endTick = tick;
+}
+int ProbeLine::endTick() const {
+    return m_endTick;
 }
 int AnchorNode::pos() const {
     return m_pos;
