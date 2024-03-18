fb6c9b221ef533241f56d91feb4a1c7fd9b066d7
笛鹿FlutyDeer <flutydeer3968@outlook.com>
2024-03-18 15:45:55 +0800

    Add divider

---
+++ src/app/UI/Controls/DividerLine.cpp
@@ -0,0 +1,22 @@
+//
+// Created by fluty on 24-3-18.
+//
+
+#include "DividerLine.h"
+
+DividerLine::DividerLine(QWidget *parent) : QFrame(parent) {
+    initUi();
+}
+DividerLine::DividerLine(Qt::Orientation orientation, QWidget *parent)
+    : QFrame(parent), m_orientation(orientation) {
+    initUi();
+}
+void DividerLine::setOrientation(Qt::Orientation orientation) {
+    m_orientation = orientation;
+    setFrameShape(m_orientation == Qt::Horizontal ? HLine : VLine);
+}
+void DividerLine::initUi() {
+    setFrameShadow(Plain);
+    setLineWidth(0);
+    setFrameShape(m_orientation == Qt::Horizontal ? HLine : VLine);
+}
