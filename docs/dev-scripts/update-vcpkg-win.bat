18599bd1916293de3db83e26258b461b23596399
笛鹿FlutyDeer <68263367+flutydeer@users.noreply.github.com>
2024-09-22 12:37:56 +0800

    Add vcpkg update script

---
+++ docs/dev-scripts/update-vcpkg-win.bat
@@ -0,0 +1,10 @@
+@echo off
+
+set QT_DIR=C:\Qt\6.7.2\msvc2019_64 # 根据实际情况修改
+set Qt6_DIR=%QT_DIR%
+set CMAKE_PREFIX_PATH=%QT_DIR%
+set VCPKG_KEEP_ENV_VARS=QT_DIR;Qt6_DIR;Qt6GuiTools_DIR;CMAKE_PREFIX_PATH
+
+cd vcpkg
+vcpkg install --x-manifest-root=../scripts/vcpkg-manifest --x-install-root=./installed --triplet=x64-windows
+pause
