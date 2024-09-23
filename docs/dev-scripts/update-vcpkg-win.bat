33f44c57fd194567e49e8687bf857a2775ddff1a
笛鹿FlutyDeer <68263367+flutydeer@users.noreply.github.com>
2024-09-24 02:35:41 +0800

    Update update-vcpkg-win.bat

--- docs/dev-scripts/update-vcpkg-win.bat
+++ docs/dev-scripts/update-vcpkg-win.bat
@@ -1,6 +1,7 @@
 @echo off
 
-set QT_DIR=C:\Qt\6.7.2\msvc2019_64 # 根据实际情况修改
+@Rem 根据实际情况修改
+set QT_DIR=C:\Qt\6.7.2\msvc2019_64
 set Qt6_DIR=%QT_DIR%
 set CMAKE_PREFIX_PATH=%QT_DIR%
 set VCPKG_KEEP_ENV_VARS=QT_DIR;Qt6_DIR;Qt6GuiTools_DIR;CMAKE_PREFIX_PATH
