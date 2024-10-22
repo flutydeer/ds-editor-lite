fcbd2ee793fa4ac814de3910a75384e975031822
笛鹿FlutyDeer <68263367+flutydeer@users.noreply.github.com>
2024-10-22 22:21:57 +0800

    Update InferEngine.cpp

--- src/app/Modules/Inference/InferEngine.cpp
+++ src/app/Modules/Inference/InferEngine.cpp
@@ -189,27 +189,27 @@ bool InferEngine::inferPitch(const QString &input, QString &output, QString &err
         return false;
     }
 
-    constexpr double timestep = 0.01161;
+    // constexpr double timestep = 0.01161;
+    //
+    // const BasePitchCurve pitchCurve(segment);
+    //
+    // std::vector<double> pitchPoints = pitchCurve.GetPitchPoints(timestep);
+    //
+    // auto &[tag, sample_curve, retake_start, retake_end] = segment.parameters["pitch"];
+    // sample_curve.samples.resize(pitchPoints.size());
+    // std::copy(pitchPoints.begin(), pitchPoints.end(), sample_curve.samples.begin());
+    // tag = "pitch";
+    // sample_curve.timestep = timestep;
+    // retake_start = 0;
+    // retake_end = pitchPoints.size();
+    //
+    // qDebug() << "Pitch Points:" << pitchPoints;
 
-    const BasePitchCurve pitchCurve(segment);
-
-    std::vector<double> pitchPoints = pitchCurve.GetPitchPoints(timestep);
-
-    auto &[tag, sample_curve, retake_start, retake_end] = segment.parameters["pitch"];
-    sample_curve.samples.resize(pitchPoints.size());
-    std::copy(pitchPoints.begin(), pitchPoints.end(), sample_curve.samples.begin());
-    tag = "pitch";
-    sample_curve.timestep = timestep;
-    retake_start = 0;
-    retake_end = pitchPoints.size();
-
-    qDebug() << "Pitch Points:" << pitchPoints;
-
-    // // Run pitch inference. The segment will be modified in-place.
-    // if (!m_pitchInfer->runInPlace(segment, &s)) {
-    //     qCritical() << "Failed to run pitch inference: " << s.msg;
-    //     return false;
-    // }
+    // Run pitch inference. The segment will be modified in-place.
+    if (!m_pitchInfer->runInPlace(segment, &s)) {
+        qCritical() << "Failed to run pitch inference: " << s.msg;
+        return false;
+    }
 
     output = QString::fromStdString(segment.toJson());
     return true;
