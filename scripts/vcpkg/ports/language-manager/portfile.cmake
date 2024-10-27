920558bdcdefb8dc76d6c93d872596c1e6d0d750
wolfgitpr <133209402+wolfgitpr@users.noreply.github.com>
2024-10-27 12:59:04 +0800

    update langMgr

--- scripts/vcpkg/ports/language-manager/portfile.cmake
+++ scripts/vcpkg/ports/language-manager/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO wolfgitpr/language-manager
-        REF 33f8ed5a2d667b1e343fbb9e1ee2d497c64fd90f
-        SHA512 11c3f58fdf6bcbdaff22266d65cb978370109de8b8c03232a273a034de10f2ce0f77aa3bcce225ef0dcb6cd71d4e7ebffa9f76a2295ad355929f96058de01bf3
+        REF c92ae31cc5428e5ff8cf1ee702ae2309ab57b640
+        SHA512 b9fa15bb7a87c4586746b16dd376f7393179d7f5dfc1c4b892a3276c264454bfbfff2f916b11a15e9da436e298406dbd9349af18d7c6117b285454fae638960b
         HEAD_REF main
 )
 
