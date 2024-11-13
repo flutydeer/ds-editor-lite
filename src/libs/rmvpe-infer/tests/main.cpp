06bd8638519a6e149beea69eeed65af60708e25e
wolfgitpr <133209402+wolfgitpr@users.noreply.github.com>
2024-11-13 16:39:30 +0800

    rmvpe add csv output

--- src/libs/rmvpe-infer/tests/main.cpp
+++ src/libs/rmvpe-infer/tests/main.cpp
@@ -23,16 +23,39 @@ static std::vector<float> freqToMidi(const std::vector<float> &frequencies) {
     return midiPitches;
 }
 
+static void writeCsv(const std::string &csvFilename, const std::vector<float> &f0, const std::vector<bool> &uv) {
+    std::ofstream csvFile(csvFilename);
+    if (!csvFile.is_open()) {
+        std::cerr << "Error opening " << csvFilename << " for writing!" << std::endl;
+        return;
+    }
+
+    int timeMs = 0;
+    for (size_t i = 0; i < f0.size(); ++i) {
+        csvFile << std::fixed << std::setprecision(5) << static_cast<float>(timeMs) / 1000.0f << "," << f0[i] << ","
+                << (uv[i] ? 1 : 0) << "\n";
+        timeMs += 10;
+    }
+
+    csvFile.close();
+    std::cout << "CSV file '" << csvFilename << "' created successfully." << std::endl;
+}
+
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        std::cerr << "Usage: " << argv[0] << " <model_path> <wav_path>" << std::endl;
+    if (argc != 5 && argc != 6) {
+        std::cerr << "Usage: " << argv[0] << " <model_path> <wav_path> <dml/cpu> <device_id> [csv_output]" << std::endl;
         return 1;
     }
 
     const std::filesystem::path modelPath = argv[1];
     const std::filesystem::path wavPath = argv[2];
+    const std::string provider = argv[3];
+    const int device_id = std::stoi(argv[4]);
+    const std::string csvOutput = argc == 6 ? argv[5] : "";
 
-    const Rmvpe::Rmvpe rmvpe(modelPath, Rmvpe::ExecutionProvider::DML, 1);
+    const auto rmProvider = provider == "dml" ? Rmvpe::ExecutionProvider::DML : Rmvpe::ExecutionProvider::CPU;
+
+    const Rmvpe::Rmvpe rmvpe(modelPath, rmProvider, device_id);
     constexpr float threshold = 0.03f;
 
     std::vector<float> f0;
@@ -49,11 +72,11 @@ int main(int argc, char *argv[]) {
         }
         std::cout << std::endl;
 
-        // std::cout << "UV output:" << std::endl;
-        // for (const bool value : uv) {
-        //     std::cout << value << " ";
-        // }
-        // std::cout << std::endl;
+        if (!csvOutput.empty() && csvOutput.substr(csvOutput.find_last_of('.')) == ".csv") {
+            writeCsv(csvOutput, f0, uv);
+        } else if (!csvOutput.empty()) {
+            std::cerr << "Error: The CSV output path must end with '.csv'." << std::endl;
+        }
     } else {
         std::cerr << "Error: " << msg << std::endl;
     }
