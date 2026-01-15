2f1549f69290f82d3facd8e1de577dd2c0c88adf
笛鹿FlutyDeer <68263367+flutydeer@users.noreply.github.com>
2026-01-15 20:58:46 +0800

    Add final states

--- docs/Piece States.ts
+++ docs/Piece States.ts
@@ -6,7 +6,7 @@ interface Context {
 }
 
 const pieceMachine = createMachine<Context>({
-    /** @xstate-layout N4IgpgJg5mDOIC5SHEnQrdaDanQgAYDpAVxoMBdA300H9UwNjTUBiNQaojBvz0BM0gbQAYBdRUABwHtYBLAC59uAOw4gAHogBMADgAs2eQE4AzMrkA2ZQEYArMwDssgDQgAnogC0q1Ur3Tle28s2H3hzQF8vZtFjwiMkpAZ-TAAl9AWBVAB89AF7NATlMWdiQQHn4hUXEpBCtnZWxDI1lpQ2lpTWZS5TNLbNVZbGdy+VkHTTLlQx0fPwwcAhJybEAAfUBpzX7iCkBvuUBCpUB6FUAKpUTxVMFhMWSsm007Ur1ZQ1VmZWVZWU1TC0QNBr0DVU09VwdpVS7fEH8+oMHAY2tACnUCFNpoAEI0A+UpLZIrdLrUCbMp2eSqYyGeTyfTKNHVRCvbBOfTqcqGDpuPTdD69QIDVDYf6AmZgxg6JJcXirDIbazSPSafKvTRohTMfnyblY2r1RrMZqtdqdMmfSnBGkA-BAwCMmoBSWIhLLSa0ynOY2Huj25nW0LSqlwQOn0uNuUpOwvusmU8op40GJEAmKmAe+iKIBZxMAq9HalKs6H67I6DHYaSonSqeTMblFREXGqlaT5Qx6A78006BSyN0BD3UwCS3oBQ-UAU4mAPlMKIADZUAFK6AY8imGxlmG9RyEA5sEVbDnpMwozp5G4xTYJfckxVTsY5PJi18QoANrPIVFQdDbzNDuvZsJkCiUaicqgM2dclpq50zpxtTmk+heJyXgVX6-C0XiIahXYP2U0HRDSRVNZ20ZhZFUMVEUMXEClkfEzkA7ZXwId9KEACwjAAbnQBspUAIeVAAdTb920hTt90kawx0zNw2hFDoZxzTQxUAxRlClR8NFKM8INQ-B0IocISGIndf3IzZAOAlE6jAtiEOY0dcXYqMM24ot3gVNC11CFdAFNrIiEhInU2RhCjslyfJClUF5ilkIU9GYoo+wQ0o3CMbNEV49DhjGIIgQWH8yJMzYzx0BoTg6Q5YwUNomKtIpYOMHRH1s2M408rTlTpEFwUM3djIjKxY1ChCjgcBMhzYq9EGzWC9htRE2ILEo1J6Es+Iy2lVXpcEmQ7Pcgs5M9sCAsd4KHc0HAc29nKfIw9ncdLfhVdUtVy0SBuyWNsGFZhp0Aol7m2MUWkzXYimiupE1URby2rOsm1bAL+ojXt+wTQCHBdREpqcpL41Rc8xxu7BV19ANgzWwKCqjRRUp0NpHn5No0yuAp8k0DGXSsjEE2u9T3XwQBGVUAVZsPzCISDJEqHuxyRygM8S8KncPR5DFJKINxMco35eMExa8k2pJ9caAYJ78u7BdjzqNjnmTJM2cRTMkWzFmMQqZxDF4oXQkiWJKb68X-ysG1bOG4dHERZHh1impR22BpmcvTipU1-HBdJzDcMI4SDfDGmVB5WzPAgjoXVuHQ2f0eptDUBd1COJEtY9gSV29-XSOemnzIKecSjKCooKteNAO2jFERNM52iT9cSb0n2M8N0yrBFPRhs8WMhTcAo5EMBWrKNZnVcTLlE7dr5tZTuv06Mv2jfUBKWaONoIJcfQ2aHeoMUA1w0VsZpq+pUYCBJvzFkhzO5+0bBmClSCXnkEfUTZ8pQr0O3DjOJEb+8MfAm1zKurTE1GLWeTchxGgeE4Nw8MTiPHjM-cBo5sz8gQroTopJf7Hw9gAoEDJz6NzhOoGMQE1DbCcMYU49ki4GFvMcB4ZCxwvkwUTbBnVcE9SphfMBcg+zQO2OxBQTw+5K0HioYeGsD7YBJmDIMIC-xNxhjGOMN9g45lcGzPO19URnm0BeBwrtWrj2wZWWsDYWzbl9vIrIKgjRvxRGcJwkECxVWtIrAeKsxHq1HoYwIgAHjUAGbawtNyi3waArIksVDqGcOeR4vcrSxk6EadWJwCTnB-j4ggASyYUzkWJawagdgKAcEcAsrRC7plTEaF0zlmhJiRrxLJOsvzTzymE6wJtDRASSjjK2o4xRlALEksoKJnglCMA0wJnt8L6VyRtY2QE7D7VOA8YcOZHj9NuK3WyCFXhJU8KidJAsviNJTmnWZBUWiGmTMUMRUYNBUPTJdXhF55AFAaouZhJySZnNCVY6w2dCjFBcgXfpaIRELKJIieGIoJnrkAI+2gAyvXrjPP5tQ3DDTqPcTwBwILFA2Ycba6tCRnCKIcjS+ATkBKnuc-25Q+yFEgUjYcoLEmRUeJBVwaTYXaWpb8vJkZhzFRzIWbMhxHjSFBWoAeSIdDuBvsrblFBa4zL5XM9uMZCiJmcMOVwKMEAvFsNtF0dtBEOO5d5TJ-jT40rniKGMZpzg2jkM4cpMhtiKGzFyU4RguTnnNWw7qNqwFEMfAnMhwrTgSvidqwlXIxkdDUEDT5kycGBt6g3Npm0yi4jPGxV56gRS+pZaFNlKTOVnH9ctGYwDVUFXAcaGWSZjCYxcfCD1ZSu6QTuOakx91zFBqyM4Psb9zgnDjPyJwxahnstSRW5NgwAkyIhpwgh7ToxwwRjvZG-TRXX06EKCCSV9g5lfJWQAe2p1lGBegdnJIL5ADiOKUKhub9KKryc4JRtgFFsNIU9FYL2nJVSuzNhVXDDQfboOoBwNCsyLvsOwzghxSleG-VwBijnYHPXWbC0zkWtNRfM3a21ZXLIMPoTw-S-rX0eJbKJGMuR-oA4JYgeH1oFQKTGIpFVSnwn6TfTpIyEw2jcFxRjdZPx6xvZGWVeQVAHCHAYFQexKMuj7Hc2cQ4iS3DExQQAb3qAH34y18RABccgAHRAIAejNzNScKvGa+sqmrHADkiG2MgCwloLTosoXIPk+Kw5PIDlj+XG0FcO4wzRRVJl0HxnMSjhOdBbt-HTyrWPUyNgc-ICmVlAQxq8OJtt4MNCsjfKFqG3A6ZFhYjNBGbGRU7jmerpw2YdFbuUR0jw2J1DkK+C9gBGHUAFd+AlyYsZaWxmmHGFzFN0CdI6Vo8v1ELYlY4ngkE9ZrANwDqWuGbEuQ0G8KIMR3K5GKV4pR7A2U60vPGPi+uDcqzZloGLjC5BchJKNNRTuZkWy6XaS8XhrY2xJrbq7pOmy6Rbe4chhywY+2eRZn0nwdAOG8G763Bs4Z+cBgjNoiNLLOGRtZLjsat2bf9GytkyUUlu0q4mmOgtzLPLBPhI4aMHBhzIY4r8zjw12rYBMAPBtUsC9V4L8IitWQMLo15WN+mkOvh+5bnrAIC4oIi4HIGo5GijAYfjImSgPI57oBo3PCTI4tir1cvKsfBa6UK8Lx6xUaBO8+c7CFsxHBFEKFXKXRtpabuqzTgptXcz1Yce2kV3Bh65C5V8MRAANpiQCggAY7WBFJgwrW4F3mKB-FmJ3Qp4hZmcKULNPBWVjwniYKfGTW42ueY3iZtDxgLHId7VxMybN2oy5412MPx8T1X6QNeXpGHrwWBMu0j1njZo8Wx5GEyuGQ+hhU8fVzJ9T7W7sb8M+QdRAUVDYoarG-qiiKSjhXS-xXyuNf1f6cvVtJA-6rykxRmOnauQx-biogfr53vcfV8D6H033vzgQTCf33ytFjlxAQiQXdU2XLxPirw33-AcG33+hinOCsmnwqD7DoXVnghxngOJmv3TRRX5RQO2hAKokxkwOoTYjCnLXcFjGKEIOv0H1v27GaEUDaAoXUAtFKDXiLkxlLieCYIeFsh72XzjwQNT1UEAP-AflghFEuhcySkfAN31VKEUDuA0AQlOHzQkIpHjwCWvyQNMmiQoJ33nAUAOHxUNHKF9V5k4KlHL2MKrxIPwzIKGgfwTCsPzX6UAnqDZTfjRCUzKBcKtQAPYOQK8MoN8JsPiQKB5BvgcKUzsjxneBEG4AgDgHEE+CiIUVKHqHBx6Sh3hgnGzwxVTA8EdW02YSpHyM2A61LgRiHFlXKAxgnFKT7Fxl+1+woR-3JSpAtSCAaP+TfmaOj2HGDg6MELyBUQcQeFsApwwQyW+GpE6lGKzXqESKWU6AqlmxqGuH0EUzfiSnKgqF4iGO9B9E2NswgXZW5C5m5G0HUIzAHhzGKzuViQGIJiGN7U2PGLei5GOFQRKAjitCsBOCKwfmb0cHKl-V-kAGgvQAeL1AAgfUAA49TYlEQ0RwW4D9GE7-To3sRMUcaSSqMofmBUQAAFTABB602Jll5BKEqhSSgyJK+2hzJKOApJuluLEIHlAi0FknBJqH0Q1TRH40fHKDPGBiPhGJFzmXDgmNsluBKBFDFA0Fgh9XUCiU6DLmBg2PlLrRHyMF1V0HsKYOOh4QfkYluGKCJA0GBn+MNKAL7ANWeOsiSjHHki0KlALFeVPGHmBlBhuOdKNkfHuNgSRn0AxknQgLRkOi7hqk8Ep3dnIFuKaLQWOF2DHWOyLgqKc3eRQJUUkVlJJnTPGMzM4nZWtOfntjkDQQqHjBBIRNWP-gNNILmU0z3VNJ500Nb2tBzASjthNjRGbRTKMUGGkRDI7LrRLUgRaCjOeNjNtjBO2m0RFETAfgUB+NTNulrE2OaFn0SnsJZm+iLnuFCgtm-zqBeLnVWMaXTOOFxBkyrJzNeNeHqGKByzaBg2MAMLakaWGICXLPzxfOzIUFzPTDNHtSOAKH+ntCXwJkAvbI8M7ONKJFHV7ItPiQkjtHaF2lRCOA6B7TugBNvBHTaCHC5BtBoIqSTAdhtDfk7jUPNUXWnNQtnIjJeBKmz3PB3V7GgQdFxV3zE1uJb3A0cVsGtg4mazRBjFOOHHOm-UQoCFuzEpsRdUKOHGdDzhOyFEzDYjtJvjUD2BaHLxIE2McENCc20sMuKXZwQBxHDhZkL1cALEgnL1XEsroJsqgwLDa1zytEP3c2jIxjWWcEIO8usrVj8qL3UWoWwJUB3maBeAqGIovzj2AtDNMistxBiv2H8qXlsI1S5HKBlAxh8B8CAA */
+    /** @xstate-layout N4IgpgJg5mDOIC5SHEnQrdaDanQgAYDpAVxoMBdA300H9UwNjTUBiNQaojBvz0BM0gbQAYBdRUABwHtYBLAC59uAOw4gAHogDMAFgBM2AGzyA7NICcatbN3SANCACeiALTSAHNgCs0pc1XW1zDQEZZm6wF8vhtFjwiMkpAZ-TAAl9AWBVAB89AF7NATlMWdiQQHn4hUXEpBFNbDWxVBwtteXt5eQ1DE2zLGztmWQsnFSUNVVcfPwwcAhJybEAAfUBpzR7iCkBvuUBCpUB6FUAKpUTxVMFhMWSs8yVpbDLrC3UXDQsLJQtKxC0ba2tmO2sNJSd5aXbfEH9uoL7AY2tACnUCcYnAAhGgHylebJRbpFagNYlTYeVS7XSuW6yVynBBPbAaaxIzT2VStJSODqvLqBXqobA-P6TYGMVxJLi8JYZVZmeTWJT5J5KFEWWTMHmyDlo8xWWz2BpNEqtZ6dAIjL6-fD-QCMmoBSWNBjLSy0ybOY2BuWMJrhajQqxkQriRmMu9QOgrsFg0xLeZOC2BIgExUwD30RRALOJgFXozUpJkQ3XZVwaWRbRFyZgcooeE4WhBlRSqRzqHkctp8iwu0kKimASW9AKH6gCnEwB8phRAAbKgApXQDHkUw2AtQzrWQgnNgitJpNZVPJmJH3ISRTVxfHmPJDvDp7IC-KPhSYoAG00AjDqAK78KIAY7QBwfB7ahiEemynvbug+xrQFaIxWKRKOYRx2BXcC-eIUAG1nkKioOjNhkQ21Fkj3RBRlB0WwrVcOo+zRY5FEOVwykJBxHA8d9Ai-H9wmieJ9zbEDJDMJRXH1aRVCTKcjg0J8DBTOFMQKCwcSOUiNkwghsMoQALCMABudAGylQAh5UAB1N8JbMFCMhYjslkFRlFUFQhWvK5+yUeD3Exep5EjNM+yfTj8G4ihwhIcTAIPIi1lI8jKMsaiWifVEU1I6NaIUXSyn0-MXldLjv1CT9AFNrMSEgkrVmWktZcnyQppEeNQLAFax4KKbsWJQgoM2sDDfMLIyAoGYYgn+WYCOAqKzD7VwbAOVppCnBo5JKNEilUfIMtcJL5F0FFDO4yklX+WlwqAyLw1MHqapYlwnDkQdaPNKpMxsFj3E0IddmnfrCqpZUaRBelWwqibHmsbAyLk5jB1NJxUsQzqnAcF9VB2xVqQmdVyvGjtJujQVrnsUj8TsDZWq7bYij5DY+WuN6Swrat6ybb6ww7LsezkUinCdDx7vS5CYN0K5KKUeHsC-H1-SDUbLMqiMoxjFEVFuHkVGTKpr3yJQeadeKozkaRDMARlVAFWbHCwjMsKLKkiadkUMjFLuWi1HQtFkKfTE5MjHkYLkHy5XeMWfxoBhUcPGT5ogyioKtOpZHVjxFAoxwcqjKdbFevLF2N0JIliaXjp+0DTCtJKLqHcoPHZod1JTEdNn7Rxla0QdZC9w3Al9ih+OE0LzassxZDubsHHsJ14SxbF1aRKwWg0eK+Q2huM5JH3xcC0TzKDtGQ5irK2pQs91bsGqWj0W4dmaZ1vaNjuKDFkLu8kk7fqFc7XEUnqBUJApp1UR34oNbK3f5dkKJF+evyXwOV+DmTzFafIcpcFQ6M0JF1cHKwo1Iu4UV7A0S+fQhgEDFqVOYtNZa-TsHkZg9QLDxXmrYXQ6t7A1WxHJBqRwKLwLJrPLOHdBofS+lA1eIdBwGnuEaNot03D0SqCaSh7hHA8hYm4No3gCFgKIXtYaIIyH32hJoLYZEG4bCxPCQ4KV45XEQhoO4HJNByQOMAikfCDp0hluQh+ZQrBHFoQ1BQfJFGH2difYuZ9PZqOwGLKmgYC701DozHqj4y7xnxHHRhA5FD1A8ByVok81A2LLFWWsjYAI9wtlkYuBpsSURojsJ4TozHH1dpYj2F9uH4EAA8agAzbRNn+M2gje6W3AioG2Tw7b2DgimHqbQDQewOLiY4+DM4EHyRLKWjiJoN02HOJ6bhGgwjRD1GolgDjIQaPGNmhlOl+zwrfCKpS1hh31GRZCAsY7uFGaRKwDUSg2zKAOBwcyCm8UEl3JZY0VlmCtNcZQm9Dj3CHP2W4ozLjnSSixKpahBRtLbu8eZJlPxXJ6b9Ro+oEw6CjJGLQMiqiPD5N2QkyICgeCjGcn8YswUlOiWYfuhRiilAYceFE5iyIUQFiaIUWLKCAEfbQAZXrL2Wfi6ohILqWDsIpPYbUPkNWUB7PERwigAr8rk85FB8k33BSHOS+okrwmoWzIcoz041QObcRBdxWl0pBTKvFhcIxDmmv2PMjgGq3HkGqhux8KKbwKNcDMerF750NU4reWxCj8lsEOO4HNjy9k2McNwckTE0TpUVDpeSIGyofnIc6agTTHCtNOWwpLUwbGjI4dkhwHDshJpGjRgIBHaKEWyEROkXB2E8FIoo-L9SlH7AtEGcki1DU0UdO+tzsgoUxH2dy6goxOEuGqhpmrmk6qOO2khGp3WnXItQycZreZLWPFm5+MJd6IKuELbJ8zsChKRhEuNMS8jVT5D1WwLE1Jjo1U07VvMxX5QPfk+xNMy09ucdGVxTDWbNQDamC12AHBkXLshXY-ZMJlkAHtq1YhhwdPWyRB+Ri5-0usXHWoyppcmOAODYBRezyGg6WODILcWfrZZNEu7g7jDksEOnq6tdiJ3ivAjwSIdat1dLB6sucKNRKNaHSljypEvKRIpUZhMQPIhrUaFmJGyOmWICym5VG+lbD5IMrqTQM2DngRdSpKITQ+L3ZnXjJl-aqbphNK0+JsDF3UJeO0OwpNOm7HC6ig58SXEU9WQAb3qAH346N8RABccgAHRAIAejNItId7TBEDm8ur4ijHcCiXjjxdQ1covszR2TzgIRZ6+brKNCY2aa+EDQLXxjcKMjxMYrTGnXngvzC9RYGtK043QnITkciuGBjYm9mPqFqPpjjV5CStdNpE7tVHYkHJ3v2Rbhx1aBMFfaW4tFLDbQIXBzclnunzt+hpgZC0dMjJTE8eEDmnDwjhYpFhmE9tbi-AJ2bQnIU2AQpRWFCj2S3mQooIUiUtuvzM4C7Az3fz-ji6YRonL4S5BQjZa1l3Ac3ZB9cV+jwnuVn27hAOsO1kR02dHacQ4HaXb7MGnGSJygZieLj-b-GSuCacfc4NTznyvOTreco50lVE0SklZ9AQoc4tZ+9pxfZ2rGnQ2iuQoyFEYKOCaa4QaCuZyh9KyXrKhMwlqPFK4LRKKVz0+IkDeGFHZRKLKCHUOmXWegSHWuBpIxXHgQKAcA4EXHmV6tdiaEnhRyZy94Kuu1NlZNd2M1VXfVWr5yI4HLFHAuCFAKUPbWOts4mp67zsNfU60Aw1DYjTVYl-ZChTCq4SA7j3Ed0CqllC3C6hlXlOUAc2gfEceoOVFLxWryuWvu4tE5-Rg4Va-IWgwS6tOVHnNFCfMBqzB44PXQ19GCP+QnXwwk0n11OMwed3q1uHEiTcg7j1CyZnVcX469xexOdahRN07xkjGiFaOwrTpx--zGeN+Vw78R8u09d6ZH9m96FdACgrxWohRNNv90ldAepB8gC9xt8x9G9rRn85BX8YCUwG5FBJkWEs1PlB9wER8G8ZIR0ICiZmhjh4oT8pxuwFEm1mIBYyDRZ78QDI8wD2QaCsYSh6D59LRbgoUWC8MBwFBuNSRVxyC0Cd8OwmprZDhNAzQyhP545eZlAWCnAFB7gko18ZCVw5DGBpAFDQIpCbtLA4RSJ1DfdUwyhoxd0tAWJDh05exB98l79KCshbAn8W85BZw+R1AG1BUC09Ymp6hPCY1gDzCqC+x+D04oZ3Ddkuoy9Lh3BaMmhoj790Cpdd8EjsCki5wQi6kChOR4Fwji5jdbByZVx9sKC4isgTwQNHgON8Q4VSIP8dgHMqkMxWgMwkIbF6itxGiMCZIWizx2i3AWCTRGD5FL8nZkI+xI0RjvCmjA1Tw2irsZi6MMtUxfUwiv98REw90XgRBuAIA4BxA3hxjVk9ESco47Byc5iUxJoRshcZl7gShLhNcIciw7iCUFFtCWZBxN4gZ9jQ54dEEPIG5sQWhQZDJyQo0ghATsgMiQTK8hxFId40E4FCgWh7hewRcuF2klxiF8A0TJppxYoeYnk2gFowZ8CFZbQhRU15opwkT3QvRvQqSdIqEtUORtYOQWh7C0xj5+w2M4VbhKIuS+gj00TsRuwTx2QFF2EBxnIqhTADhagPIihyh5piNslkSRi0TexlTewyJ4xdAzQD5LtyJXl+QSSvdpCAhABoL0AHi9QAIH1AAOPTRMon1H5wTEoh0gUEpy1Lpwcwp3skWhKANgh0AABUwAQes0TaIBcKJyh9hEEGMRRIz+R1okpYzjh4YqT9Dj4qJpxHIWI0RbsvVeRuVygZxyZQFUT8jfoMTCTydLhJD7CtB2p81NBa1IxdByY9o+SJ8HB-U3BSgephCEAoYHN2RFJLhVY-tyYFS2zMCLTcsVB4opl9jXIbB6gup04sQ4wjSySBpKZeTNzdENVqFGg2YkQeYsQ0QuZQZd5MxFJRc55yAqTNtMRN4-tVTL17DkIRsFEkD4oVU2gbEWyxZ-ylSOFgKtUFAwKeZ+knRN4pwYI1SLz-j8BfYKTxz+zPEsKZyFAT8Mx8gRww4UQlUfzCE+g7EbzQDTp7zBSnyRTXz44NTlA9AhR+RjERzskiKNy2LFD9l4kWIm09AR5SItgowwzLBRTp191zl-zgTkLU5UL-s6knhzpQM7M5Auoy5I0Wz8lEKaptLtgDg0LRlaEtgChrwiZbRXSgVzliLbzoQJyyLpypxZzUj9QsRpRrhdB9h3LAgD1xKeDwwGgz8VZvcdgp59jt4KU6LdZbhI031WLYrfp+TDQkUyI1B9I7TEUc0FJLpaJEx04-M+S0osjszY4dI10EBhyap2R7lZ9dgnVQ8+TYl009EhxHQfFbwBRCCpwMx4E4TpFB8SAzTYlwKPJ4R8Qpw7hO8MicoconQ9loYUDPwFq8hwKcpyhSchQM1HArBrgrQcx4w1pIrZDRZDqLo1ATq3Ao5zr5iHMWCbTHg1qHqVxLLvKZBFrXqFB3qlLbBQjBx2R7ApQeY6j1wNwzTNhMY2ZvlL1ujzoG47NlzlyTRhikaUbtyzy7rIKwL+sYxFjjykRSSCKD1TTgb0RyJsQihqI5pwKPkaSQqv9WamEzivAgA */
     id: "片段状态",
     states: {
         "推理时长阶段": {
@@ -22,7 +22,7 @@ const pieceMachine = createMachine<Context>({
                 "正在推理": {
                     on: {
                         "任务成功": [{
-                            target: "#片段状态.更新时长",
+                            target: "更新就绪",
                             cond: "文档模型未占用"
                         }, "等待释放"],
 
@@ -40,7 +40,15 @@ const pieceMachine = createMachine<Context>({
 
                 "等待释放": {
                     on: {
-                        "占用解除": "#片段状态.更新时长"
+                        "占用解除": "更新就绪"
+                    }
+                },
+
+                "更新就绪": {
+                    type: "final",
+
+                    on: {
+                        "完成": "#片段状态.更新时长"
                     }
                 }
             },
@@ -75,7 +83,7 @@ const pieceMachine = createMachine<Context>({
                 "正在推理": {
                     on: {
                         "任务成功": [{
-                            target: "#片段状态.更新音高",
+                            target: "更新就绪",
                             cond: "文档模型未占用"
                         }, "等待释放"],
 
@@ -87,13 +95,21 @@ const pieceMachine = createMachine<Context>({
 
                 "等待释放": {
                     on: {
-                        "占用解除": "#片段状态.更新音高"
+                        "占用解除": "更新就绪"
                     }
                 },
 
                 "音高错误": {
                     on: {
                         "重试": "开始推理时长"
+                    }
+                },
+
+                "更新就绪": {
+                    type: "final",
+
+                    on: {
+                        "完成": "#片段状态.更新音高"
                     }
                 }
             },
@@ -125,7 +141,7 @@ const pieceMachine = createMachine<Context>({
                     on: {
                         "任务失败": "唱法错误",
                         "任务成功": [{
-                            target: "#片段状态.更新唱法",
+                            target: "更新就绪",
                             cond: "文档模型未占用"
                         }, "等待释放"]
                     }
@@ -139,7 +155,15 @@ const pieceMachine = createMachine<Context>({
 
                 "等待释放": {
                     on: {
-                        "占用解除": "#片段状态.更新唱法"
+                        "占用解除": "更新就绪"
+                    }
+                },
+
+                "更新就绪": {
+                    type: "final",
+
+                    on: {
+                        "完成": "#片段状态.更新唱法"
                     }
                 }
             },
@@ -172,7 +196,7 @@ const pieceMachine = createMachine<Context>({
 
                     on: {
                         "任务成功": [{
-                            target: "#片段状态.更新声学",
+                            target: "更新就绪",
                             cond: "文档模型未占用"
                         }, "等待释放"],
 
@@ -182,13 +206,21 @@ const pieceMachine = createMachine<Context>({
 
                 "等待释放": {
                     on: {
-                        "占用解除": "#片段状态.更新声学"
+                        "占用解除": "更新就绪"
                     }
                 },
 
                 "声学错误": {
                     on: {
                         "重试": "开始推理声学"
+                    }
+                },
+
+                "更新就绪": {
+                    type: "final",
+
+                    on: {
+                        "完成": "#片段状态.更新声学"
                     }
                 }
             },
