2a14081fb6a801bb6fe6e514e8d064ad04b31ac6
笛鹿FlutyDeer <68263367+flutydeer@users.noreply.github.com>
2026-07-26 00:52:52 +0800

    Remove deprecated serialization method

--- src/libs/Support/ISerializable.h
+++ src/libs/Support/ISerializable.h
@@ -5,29 +5,14 @@
 #ifndef ISERIALIZABLE_H
 #define ISERIALIZABLE_H
 
-#include <lite/Support/JsonUtils.h>
 #include <lite/Support/Macros.h>
 
-#include <QJsonArray>
 #include <QJsonObject>
-#include <QList>
 
 LITE_INTERFACE ISerializable {
     I_DECL(ISerializable)
     I_NODSCD(QJsonObject serialize() const);
     I_METHOD(bool deserialize(const QJsonObject &obj));
-
-    template <typename T>
-    [[deprecated("Use JsonUtils::deserializeList instead")]]
-    void deserializeJArray(const QJsonArray &jsonArray, QList<T> &list) {
-        JsonUtils::deserializeList(jsonArray, list);
-    }
-
-    template <typename T>
-    [[deprecated("Use JsonUtils::serializeList instead")]]
-    QJsonArray serializeJArray(const QList<T> &list) const {
-        return JsonUtils::serializeList(list);
-    }
 };
 
 #endif // ISERIALIZABLE_H
