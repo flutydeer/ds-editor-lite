070ed2413b776e5039b2f1041a96b9567847807c
SineStriker <trueful@163.com>
2026-08-15 21:23:23 +0800

    Fix vcpkg issues

--- src/app/Bootstrap/CrashHandler.cpp
+++ src/app/Bootstrap/CrashHandler.cpp
@@ -1,14 +1,17 @@
 #include "CrashHandler.h"
 
+#include <QApplication>
+#include <QStandardPaths>
+
 #ifdef LITE_ENABLE_BREAKPAD
-#  include <QApplication>
 #  include <QBreakpadHandler.h>
 #endif
 
 CrashHandler::CrashHandler() {
 #ifdef LITE_ENABLE_BREAKPAD
     m_handler = std::make_unique<QBreakpadHandler>();
-    m_handler->setDumpPath(QApplication::applicationDirPath() + "/dumps");
+    m_handler->setDumpPath(QStandardPaths::writableLocation(QStandardPaths::AppDataLocation) +
+                           QStringLiteral("/Dumps"));
 
     QBreakpadHandler::UniqueExtraHandler = []() {
         // Do something when crash occurs.
