268159f292ba4e8936b904766e5cbb0b8a87c7c3
笛鹿FlutyDeer <68263367+flutydeer@users.noreply.github.com>
2026-08-16 15:17:53 +0800

    Use project Dialog base for about dialog

--- src/app/UI/Dialogs/Help/AboutDialog.cpp
+++ src/app/UI/Dialogs/Help/AboutDialog.cpp
@@ -3,45 +3,79 @@
 #include <lite/BuildInfo.h>
 #include <lite/ProductMetadata.h>
 
-#include <QMCore/qmsystem.h>
+#include <lite/GUI/Controls/Button.h>
 
 #include <QApplication>
-#include <QGridLayout>
 #include <QIcon>
+#include <QLabel>
 #include <QScreen>
-#include <QSpacerItem>
+#include <QVBoxLayout>
 #include <QtGlobal>
+#include <QMCore/qmsystem.h>
 
-AboutDialog::AboutDialog(QWidget *parent) : QMessageBox(parent) {
+AboutDialog::AboutDialog(QWidget *parent) : Dialog(parent) {
+    const QString appName = qApp->applicationName();
+
+    setWindowTitle(QApplication::translate("Application", "About %1").arg(appName));
+    setModal(true);
+    setMinimumWidth(460);
+
+    m_textLabel = new QLabel;
+    m_textLabel->setTextFormat(Qt::RichText);
+    m_textLabel->setWordWrap(true);
+    m_textLabel->setAlignment(Qt::AlignLeft | Qt::AlignTop);
+    m_textLabel->setTextInteractionFlags(Qt::TextBrowserInteraction);
+    m_textLabel->setOpenExternalLinks(true);
+    m_textLabel->setText(buildHtml());
+
+    const auto icon = qApp->windowIcon();
+    const double ratio = screen()->logicalDotsPerInch() / QM::unitDpi();
+    if (!icon.isNull()) {
+        m_iconLabel = new QLabel;
+        m_iconLabel->setPixmap(icon.pixmap(QSize(40, 40) * ratio));
+        m_iconLabel->setAlignment(Qt::AlignHCenter);
+    }
+
+    const auto mainLayout = new QVBoxLayout(body());
+    mainLayout->setContentsMargins({});
+    mainLayout->setSpacing(12);
+    if (m_iconLabel)
+        mainLayout->addWidget(m_iconLabel);
+    mainLayout->addWidget(m_textLabel);
+
+    auto *okButton = new Button(tr("OK"));
+    setPositiveButton(okButton);
+    connect(okButton, &Button::clicked, this, &QDialog::accept);
+}
+
+QString AboutDialog::buildHtml() const {
     const QString appName = qApp->applicationName();
 
     const QString copyrightInfo =
         QApplication::translate("Application", "<p>Based on Qt version %1.<br>%2</p>")
             .arg(QStringLiteral(QT_VERSION_STR), QString::fromUtf8(LiteProductMetadata::Copyright));
 
-    const QString buildInfo =
-        QApplication::translate("Application", "<h3>Build Information</h3>"
-                                               "<p>"
-                                               "Version: %1<br>"
-                                               "Branch: %2<br>"
-                                               "Commit: %3<br>"
-                                               "Build date: %4<br>"
-                                               "Toolchain: %5 %6 %7"
-                                               "</p>")
-            .arg(QApplication::applicationVersion(),
-                 QStringLiteral(LITE_GIT_BRANCH),           //
-                 QStringLiteral(LITE_GIT_LAST_COMMIT_HASH), //
-                 QStringLiteral(LITE_BUILD_TIME),           //
-                 QStringLiteral(LITE_COMPILER_ARCH),        //
-                 QStringLiteral(LITE_COMPILER_ID),          //
-                 QStringLiteral(LITE_COMPILER_VERSION));
+    const QString buildInfo = QApplication::translate("Application", "<h3>Build Information</h3>"
+                                                                     "<p>"
+                                                                     "Version: %1<br>"
+                                                                     "Branch: %2<br>"
+                                                                     "Commit: %3<br>"
+                                                                     "Build date: %4<br>"
+                                                                     "Toolchain: %5 %6 %7"
+                                                                     "</p>")
+                                  .arg(QApplication::applicationVersion(),
+                                       QStringLiteral(LITE_GIT_BRANCH),           //
+                                       QStringLiteral(LITE_GIT_LAST_COMMIT_HASH), //
+                                       QStringLiteral(LITE_BUILD_TIME),           //
+                                       QStringLiteral(LITE_COMPILER_ARCH),        //
+                                       QStringLiteral(LITE_COMPILER_ID),          //
+                                       QStringLiteral(LITE_COMPILER_VERSION));
 
     const QString aboutInfo =
         QApplication::translate(
-            "Application",
-            "<h3>About Application</h3>"
-            "<p>%1 is a cross-platform SVS editing application powered by "
-            "DiffSinger for virtual singer producers to make song compositions.</p>")
+            "Application", "<h3>About Application</h3>"
+                           "<p>%1 is a cross-platform SVS editing application powered by "
+                           "DiffSinger for virtual singer producers to make song compositions.</p>")
             .arg(appName);
 
     const QString licenseInfo =
@@ -56,24 +90,6 @@ AboutDialog::AboutDialog(QWidget *parent) : QMessageBox(parent) {
             .arg(QStringLiteral(
                 R"(<a href="https://www.apache.org/licenses/LICENSE-2.0">apache.org/licenses</a>)"));
 
-    setWindowTitle(QApplication::translate("Application", "About %1").arg(appName));
-    setText(QApplication::translate("Application", "<h2>%1</h2>%2%3%4%5")
-                .arg(appName, copyrightInfo, buildInfo, aboutInfo, licenseInfo));
-    setIcon(QMessageBox::Information);
-
-    // Let users select/copy the build details (and still click the license link).
-    setTextInteractionFlags(Qt::TextBrowserInteraction);
-
-    const double ratio = screen()->logicalDotsPerInch() / QM::unitDpi();
-
-    const QIcon icon = qApp->windowIcon();
-    if (!icon.isNull())
-        setIconPixmap(icon.pixmap(QSize(40, 40) * ratio));
-
-    if (const auto gridLayout = qobject_cast<QGridLayout *>(layout())) {
-        const auto horizontalSpacer = new QSpacerItem(static_cast<int>(ratio * 500), 0,
-                                                      QSizePolicy::Minimum, QSizePolicy::Expanding);
-        gridLayout->addItem(horizontalSpacer, gridLayout->rowCount(), 0, 1,
-                            gridLayout->columnCount());
-    }
+    return QApplication::translate("Application", "<h2>%1</h2>%2%3%4%5")
+        .arg(appName, copyrightInfo, buildInfo, aboutInfo, licenseInfo);
 }
--- src/app/UI/Dialogs/Help/AboutDialog.h
+++ src/app/UI/Dialogs/Help/AboutDialog.h
@@ -1,14 +1,23 @@
 #ifndef ABOUTDIALOG_H
 #define ABOUTDIALOG_H
 
-#include <QMessageBox>
+#include "UI/Dialogs/Base/Dialog.h"
+
+class QLabel;
 
 // The "About" box: product identity, build metadata (from <lite/BuildInfo.h>)
-// and license. Construct and exec() it like any dialog.
-class AboutDialog final : public QMessageBox {
+// and license. Built on the shared Dialog base (frameless + theme-aware).
+class AboutDialog final : public Dialog {
     Q_OBJECT
+
 public:
     explicit AboutDialog(QWidget *parent = nullptr);
+
+private:
+    [[nodiscard]] QString buildHtml() const;
+
+    QLabel *m_textLabel = nullptr;
+    QLabel *m_iconLabel = nullptr;
 };
 
 #endif // ABOUTDIALOG_H
