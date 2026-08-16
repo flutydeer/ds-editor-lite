4c41eece075c923b9e84fff73e6d75e215175f49
yqzhishen <yangqian_1015@icloud.com>
2026-08-17 03:56:09 +0800

    fix(editor): elide lyrics in short notes

--- src/app/UI/Views/ClipEditor/PianoRoll/NoteView.h
+++ src/app/UI/Views/ClipEditor/PianoRoll/NoteView.h
@@ -8,6 +8,7 @@
 #include <lite/Core/UniqueObject.h>
 
 #include <QColor>
+#include <QFont>
 
 class PronunciationView;
 
@@ -33,6 +34,7 @@ public:
     void setKeyIndex(int keyIndex);
     [[nodiscard]] QString lyric() const;
     void setLyric(const QString &lyric);
+    [[nodiscard]] bool isLyricElided() const;
     void setPronunciation(const QString &pronunciation, bool edited);
     [[nodiscard]] bool editingPitch() const;
     void setEditingPitch(bool on);
@@ -55,6 +57,7 @@ public:
     [[nodiscard]] bool isEditingLyric() const;
 
 private:
+    [[nodiscard]] QFont lyricFont() const;
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
     void updateRectAndPos() override;
     void adjustPronView() const;
